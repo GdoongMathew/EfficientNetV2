b3d88182567e5fa82838ca38e98979e3fc503de3
GdoongMathew <dungdudu426@gmail.com>
2021-07-18 23:37:38 +0800

    add fused_mb_conv_block(without SE layers.)

--- model.py
+++ model.py
@@ -1,13 +1,21 @@
 from config import *
-from efficientnet.model import mb_conv_block, round_filters, CONV_KERNEL_INITIALIZER
+from efficientnet.model import mb_conv_block, round_filters, CONV_KERNEL_INITIALIZER, get_dropout
 from tensorflow.keras import layers
 from tensorflow.keras import backend
+from tensorflow.keras import models
+from tensorflow.keras import utils
 
-
-def fused_mb_conv_block(inputs, block_args: BlockArgs, activation='swish', drop_rate=None, prefix=''):
+def fused_mb_conv_block(inputs, block_args: BlockArgs, activation='swish', drop_rate=None, prefix='', conv_dropout=None):
     """Fused Mobile Inverted Residual Bottleneck"""
     has_se = (block_args.se_ratio is not None) and (0 < block_args.se_ratio <= 1)
     bn_axis = 3 if backend.image_data_format() == 'channels_last' else 1
+
+    Dropout = get_dropout(
+        backend=backend,
+        layers=layers,
+        models=models,
+        utils=utils
+    )
 
     x = inputs
 
@@ -23,6 +31,33 @@ def fused_mb_conv_block(inputs, block_args: BlockArgs, activation='swish', drop_
                           name=f'{prefix}expand_conv')(x)
         x = layers.BatchNormalization(name=f'{prefix}expand_bn')(x)
         x = layers.Activation(activation=activation, name=f'{prefix}_expand_activation')(x)
+    if conv_dropout and block_args.expand_ratio > 1:
+        x = layers.Dropout(conv_dropout)(x)
+
+    if has_se:
+        pass
+
+    # Output phase
+    x = layers.Conv2D(block_args.output_filters,
+                      kernel_size=1 if block_args.expand_ratio != 1 else block_args.kernel_size,
+                      strides=1 if block_args.expand_ratio != 1 else block_args.strides,
+                      kernel_initializer=CONV_KERNEL_INITIALIZER,
+                      padding='same',
+                      use_bias=False,
+                      name=f'{prefix}project_conv')(x)
+
+    x = layers.BatchNormalization(name=f'{prefix}project_bn')(x)
+    if block_args.expand_ratio == 1:
+        x = layers.Activation(activation=activation, name=f'{prefix}activation')(x)
+
+    if all(s == 1 for s in block_args.strides) \
+            and block_args.input_filters == block_args.output_filters:
+        if drop_rate and drop_rate > 0:
+            x = Dropout(drop_rate,
+                        noise_shape=(None, 1, 1, 1),
+                        name=f'{prefix}dropout')(x)
+        x = layers.Add(name=f'{prefix}add')([x, inputs])
+    return x
 
 
 def EfficientNetV2(block_args,
