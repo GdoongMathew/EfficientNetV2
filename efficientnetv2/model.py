4ed1b850d6d7868613b5fa4696f55db28bed1e48
GdoongMathew <dungdudu426@gmail.com>
2021-07-22 20:24:14 +0800

    remove theano backend supported blocks.

--- efficientnetv2/model.py
+++ efficientnetv2/model.py
@@ -97,14 +97,6 @@ def mb_conv_block(inputs,
                                   use_bias=True,
                                   kernel_initializer=CONV_KERNEL_INITIALIZER,
                                   name=prefix + 'se_expand')(se_tensor)
-        if backend.backend() == 'theano':
-            # For the Theano backend, we have to explicitly make
-            # the excitation weights broadcastable.
-            pattern = ([True, True, True, False] if backend.image_data_format() == 'channels_last'
-                       else [True, False, True, True])
-            se_tensor = layers.Lambda(
-                lambda x: backend.pattern_broadcast(x, pattern),
-                name=prefix + 'se_broadcast')(se_tensor)
         x = layers.multiply([x, se_tensor], name=prefix + 'se_excite')
 
     # Output phase
