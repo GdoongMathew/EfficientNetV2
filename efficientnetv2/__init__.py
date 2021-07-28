b4be4d1cfdd03d1092e2c5667d3bf60b45ab5893
GdoongMathew <dungdudu426@gmail.com>
2021-07-29 01:33:28 +0800

    fix missing Layers.

--- efficientnetv2/__init__.py
+++ efficientnetv2/__init__.py
@@ -19,3 +19,7 @@ from .model import EfficientNetV2_S
 from .model import EfficientNetV2_M
 from .model import EfficientNetV2_L
 from .model import EfficientNetV2_XL
+
+from tensorflow.keras.utils import get_custom_objects
+from .model import get_dropout
+get_custom_objects().update({'FixedDropout': get_dropout()})
